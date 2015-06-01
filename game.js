06168a7fd6571bec01cfda3633fc475dddac4884
Simon Furborg <simfur@marvell.com>
2015-06-01 10:06:08 +0200

    Added way to move the snake

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ function segment(color, x, y) {
 	this.color = color;
 	this.x = x;
 	this.y = y;
-	
+
 	this.move = function(dir, length) {
 		switch (dir) {
 			case "right":
@@ -22,12 +22,12 @@ function segment(color, x, y) {
 				console.log("Bad direction");
 		}
 	};
-	
+
 	this.draw = function() {
 		//draw a circle
 		ctx.beginPath();
 		ctx.fillStyle = this.color;
-		ctx.arc(this.x, this.y,  5, 0, Math.PI*2, true); 
+		ctx.arc(this.x, this.y,  5, 0, Math.PI*2, true);
 		ctx.closePath();
 		ctx.fill();
 	};
@@ -35,22 +35,27 @@ function segment(color, x, y) {
 
 //Class snake
 function Snake(color) {
-	var segments = [new segment("#000000", 25, 25)];
-	
+  this.direction = "right";
+	this.segments = [new segment("#000000", 25, 25)];
+
 	this.addSegment = function(color, x, y) {
-		segments.push(new segment(color, x, y));
+	  this.segments.push(new segment(color, x, y));
 	};
-	
-	this.move = function(dir) {
-		for (var i = 0; i < segments.length; i++) {
-			segments[i].move(dir, 10);
+
+	this.move = function() {
+		for (var i = 0; i < this.segments.length; i++) {
+			this.segments[i].move(this.direction, 10);
 		}
 	};
-	
+
 	this.draw = function() {
-		for (var i = 0; i < segments.length; i++) {
-			segments[i].draw();
+		for (var i = 0; i < this.segments.length; i++) {
+			this.segments[i].draw();
 		}
+	}
+
+  this.setDirection = function(direction) {
+		  this.direction = direction;
 	}
 };
 
@@ -61,7 +66,8 @@ var snake = new Snake();
 function init() {
 	//get a reference to the canvas
 	ctx = $('#canvas')[0].getContext("2d");
-	return setInterval(gameLoop, 10);
+  bindEvents();
+	return setInterval(gameLoop, 500);
 };
 
 function move() {
@@ -71,19 +77,41 @@ function move() {
 
 function gameLoop() {
 	// Move snake
-	snake.move("right");
-	
+  snake.move();
+
 	//Check if food is eaten
-	snake.addSegment("#CCFFFF", 100, 100);
-	
+	//snake.addSegment("#CCFFFF", 100, 100);
+
 	//Draw the snake
 	snake.draw();
+};
+
+function bindEvents() {
+    var keysToDirections = {
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down'
+    };
+
+    $(document).keydown(function (event) {
+      var key = event.which;
+      var direction = keysToDirections[key];
+      console.log(direction);
+      if (direction) {
+        snake.setDirection(direction);
+        //event.preventDefault();
+      }
+      else if (key === 32) {
+        restart();
+      }
+    });
 };
 
 $(document).ready(function() {
 	init();
 	//var snake = new Object();
-	
+
 	//var seg1 = new segment("red");
 
 	//seg1.draw();
